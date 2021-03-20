0ebbd37d923ea6bd1595ab5b81f85017080c8086
dpopielski <damianpopielski.dp@gmail.com>
2021-03-20 22:04:44 +0100

    dynamic inputs

--- projekt-lab1/src/app.ts
+++ projekt-lab1/src/app.ts
@@ -1,59 +1,68 @@
 interface Displays {
-    sum: HTMLInputElement;
-    avg: HTMLInputElement;
-    max: HTMLInputElement;
-    min: HTMLInputElement;
+	sum: HTMLInputElement;
+	avg: HTMLInputElement;
+	max: HTMLInputElement;
+	min: HTMLInputElement;
 }
 
 class CalcApp {
-    arrInputData: HTMLInputElement[] = [];
-    displays: Displays;
+	arrInputData: HTMLInputElement[] = [];
+	displays: Displays = <Displays>new Object();
+	inputEl: HTMLInputElement;
+	userInput: Number;
+	inputData: HTMLDivElement;
+	inputValue: Number;
 
-    constructor() {
-        this.computeData = this.computeData.bind(this);
-        this.startApp();
-    }
-    
-    startApp() {
-        this.getInputs();
-        this.inputValueEvents();
-    }
+	constructor() {
+		this.computeData = this.computeData.bind(this);
 
-    getInputs() {
-        for (let i = 0; i < 4; i++) {
-            this.arrInputData.push(<HTMLInputElement>document.querySelector(`#input${i}`));
-        }
+		this.generateInputs();
 
-        // for (let elem of ["sum", "avg", "max", "min"]) {
-        //     this.displays[elem] = document.querySelector(`#${elem}`)
-        // }
+		for (let elem of ['sum', 'avg', 'max', 'min']) {
+			this.displays[elem] = document.querySelector(`#${elem}`);
+		}
+	}
 
-        for (let elem in this.displays) {
-            this.displays[elem] = document.querySelector(`#${elem}`)
-        }
-    }
+	generateInputs() {
+		this.inputData = <HTMLDivElement>document.querySelector('.inputData');
+		this.inputEl = <HTMLInputElement>document.getElementById('userInput');
 
-    inputValueEvents() {
-        for (let element of this.arrInputData) {
-            element.addEventListener("input", this.computeData);
-        }
-    }
+		this.inputEl.addEventListener('input', () => {
+			const amount = parseInt(this.inputEl.value);
+			this.inputData.innerHTML = '';
+			this.arrInputData = [];
+			for (let i = 0; i < amount; i++) {
+				const input = document.createElement('input');
+				input.type = 'number';
+				this.inputData.appendChild(input);
+				this.arrInputData.push(input);
+				input.addEventListener('input', this.computeData);
+			}
+			this.computeData();
+		});
+	}
 
-    computeData() {
-        const values = this.arrInputData.filter(el => el.value.length > 0).map(el => +el.value);
-        const sum = values.reduce((a, b) => a + b);
-        const avg = sum / values.length;
-        const max = Math.max(...values);
-        const min = Math.min(...values);
+	computeData() {
+		const values = this.arrInputData.filter((el) => el.value.length > 0).map((el) => +el.value);
+		if (values.length > 0) {
+			const sum = values.reduce((a, b) => a + b, 0);
+			const avg = sum / values.length;
+			const max = Math.max(...values);
+			const min = Math.min(...values);
 
-        this.showNewInputs(sum, avg, max, min);
-    }
+			this.showNewInputs(sum, avg, max, min);
+		} else {
+			// brak danych
+			this.showNewInputs(0, 0, 0, 0);
+		}
+	}
 
-    showNewInputs(sum: number, avg: number, max: number, min: number) {
-        this.displays.sum.value = ""+sum;
-        this.displays.avg.value = ""+avg;
-        this.displays.max.value = ""+max;
-        this.displays.min.value = ""+min;
-    }
+	showNewInputs(sum: number, avg: number, max: number, min: number) {
+		console.log(this.displays);
+		this.displays.sum.value = '' + sum;
+		this.displays.avg.value = '' + avg;
+		this.displays.max.value = '' + max;
+		this.displays.min.value = '' + min;
+	}
 }
 const calcApp = new CalcApp();
