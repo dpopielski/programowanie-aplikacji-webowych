ed5099f09e0b105609f62371d4df4d8a0735b4df
dpopielski <damianpopielski.dp@gmail.com>
2021-03-21 13:52:18 +0100

    loading and remove button

--- projekt-lab1/src/app.ts
+++ projekt-lab1/src/app.ts
@@ -1,68 +1,117 @@
 interface Displays {
-	sum: HTMLInputElement;
-	avg: HTMLInputElement;
-	max: HTMLInputElement;
-	min: HTMLInputElement;
+  sum: HTMLInputElement;
+  avg: HTMLInputElement;
+  max: HTMLInputElement;
+  min: HTMLInputElement;
 }
 
 class CalcApp {
-	arrInputData: HTMLInputElement[] = [];
-	displays: Displays = <Displays>new Object();
-	inputEl: HTMLInputElement;
-	userInput: Number;
-	inputData: HTMLDivElement;
-	inputValue: Number;
+  arrInputData: HTMLInputElement[] = [];
+  arrCheckboxData: HTMLInputElement[] = [];
 
-	constructor() {
-		this.computeData = this.computeData.bind(this);
+  displays: Displays = <Displays>new Object();
+  inputEl: HTMLInputElement;
+  userInput: Number;
+  inputData: HTMLDivElement;
+  inputValue: Number;
+  clearButton: HTMLButtonElement;
+  loadingEl: HTMLDivElement;
 
-		this.generateInputs();
+  constructor() {
+    this.computeData = this.computeData.bind(this);
 
-		for (let elem of ['sum', 'avg', 'max', 'min']) {
-			this.displays[elem] = document.querySelector(`#${elem}`);
-		}
-	}
+    this.generateInputs();
+    this.removeContent();
 
-	generateInputs() {
-		this.inputData = <HTMLDivElement>document.querySelector('.inputData');
-		this.inputEl = <HTMLInputElement>document.getElementById('userInput');
+    for (let elem of ["sum", "avg", "max", "min"]) {
+      this.displays[elem] = document.querySelector(`#${elem}`);
+    }
+  }
 
-		this.inputEl.addEventListener('input', () => {
-			const amount = parseInt(this.inputEl.value);
-			this.inputData.innerHTML = '';
-			this.arrInputData = [];
-			for (let i = 0; i < amount; i++) {
-				const input = document.createElement('input');
-				input.type = 'number';
-				this.inputData.appendChild(input);
-				this.arrInputData.push(input);
-				input.addEventListener('input', this.computeData);
-			}
-			this.computeData();
-		});
-	}
+  generateInputs() {
+    this.inputData = <HTMLDivElement>document.querySelector(".inputData");
+    this.inputEl = <HTMLInputElement>document.getElementById("userInput");
+    this.loadingEl = document.querySelector(".loading-container");
 
-	computeData() {
-		const values = this.arrInputData.filter((el) => el.value.length > 0).map((el) => +el.value);
-		if (values.length > 0) {
-			const sum = values.reduce((a, b) => a + b, 0);
-			const avg = sum / values.length;
-			const max = Math.max(...values);
-			const min = Math.min(...values);
+    this.inputEl.addEventListener("focus", () => {
+      this.loadingEl.style.display = "flex";
+      this.inputData.style.display = "none";
+    });
 
-			this.showNewInputs(sum, avg, max, min);
-		} else {
-			// brak danych
-			this.showNewInputs(0, 0, 0, 0);
-		}
-	}
+    this.inputEl.addEventListener("blur", () => {
+      const amount = +this.inputEl.value;
+      this.inputData.innerHTML = "";
+      this.arrInputData = [];
+      this.loadingEl = document.querySelector(".loading-container");
+      this.loadingEl.style.display = "none";
+      this.inputData.style.display = "block";
 
-	showNewInputs(sum: number, avg: number, max: number, min: number) {
-		console.log(this.displays);
-		this.displays.sum.value = '' + sum;
-		this.displays.avg.value = '' + avg;
-		this.displays.max.value = '' + max;
-		this.displays.min.value = '' + min;
-	}
+      for (let i = 0; i < amount; i++) {
+        const input = document.createElement("input");
+        const checkbox = document.createElement("input");
+        input.id = `input${i}`;
+        input.type = "number";
+        checkbox.id = `checkbox${i}`;
+        checkbox.type = "checkbox";
+        this.inputData.appendChild(input);
+        this.inputData.appendChild(checkbox);
+        this.arrInputData.push(input);
+        this.arrCheckboxData.push(checkbox);
+        console.log(this.arrCheckboxData);
+        console.log(this.arrInputData);
+        input.addEventListener("input", this.computeData);
+      }
+      this.computeData();
+    });
+  }
+
+  computeData() {
+    const values = this.arrInputData
+      .filter((el) => el.value.length > 0)
+      .map((el) => +el.value);
+    if (values.length > 0) {
+      const sum = values.reduce((a, b) => a + b, 0);
+      const avg = sum / values.length;
+      const max = Math.max(...values);
+      const min = Math.min(...values);
+
+      this.showNewInputs(sum, avg, max, min);
+    } else {
+      // brak danych
+      this.showNewInputs(0, 0, 0, 0);
+    }
+  }
+
+  showNewInputs(sum: number, avg: number, max: number, min: number) {
+    this.displays.sum.value = "" + sum;
+    this.displays.avg.value = "" + avg;
+    this.displays.max.value = "" + max;
+    this.displays.min.value = "" + min;
+  }
+
+  removeContent() {
+    this.inputEl = <HTMLInputElement>document.getElementById("userInput");
+    this.clearButton = <HTMLButtonElement>(
+      document.getElementById("clearButton")
+    );
+    const amount = +this.inputEl.value;
+
+    this.clearButton.addEventListener("click", () => {
+      for (let i = 0; i < amount; i++) {
+        const checkbox: HTMLInputElement = <HTMLInputElement>(
+          document.getElementById(`checkbox${i}`)
+        );
+        const input: HTMLInputElement = <HTMLInputElement>(
+          document.getElementById(`input${i}`)
+        );
+        if (checkbox.checked) {
+          console.log("true");
+          input.value = "";
+        } else {
+          return;
+        }
+      }
+    });
+  }
 }
 const calcApp = new CalcApp();
