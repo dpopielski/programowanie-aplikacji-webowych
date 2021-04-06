924dcfc6730999b0f4620ebb1751e11b53d545e2
Damian Popielski <damian.popielski@vmlyr.com>
2021-04-06 21:20:13 +0200

    sounds update

--- projekt-lab2/src/app.ts
+++ projekt-lab2/src/app.ts
@@ -1,8 +1,16 @@
 export class App {
-    clapSound: HTMLAudioElement;
     boomSound: HTMLAudioElement;
+    clapSound: HTMLAudioElement;
     hihatSound: HTMLAudioElement;
+    kickSound: HTMLAudioElement;
+    openhatSound: HTMLAudioElement;
+    rideSound: HTMLAudioElement;
+    snareSound: HTMLAudioElement;
+    tinkSound: HTMLAudioElement;
+    tomSound: HTMLAudioElement;
+
     channel1: any[] = [];
+    drumPadBtn: HTMLButtonElement;
 
     constructor() {
         this.appStart();
@@ -23,9 +31,15 @@ export class App {
     }
 
     getAudioTags() {
-        this.clapSound = document.querySelector('[data-sound="clap"]');
         this.boomSound = document.querySelector('[data-sound="boom"]');
+        this.clapSound = document.querySelector('[data-sound="clap"]');
         this.hihatSound = document.querySelector('[data-sound="hihat"]');
+        this.kickSound = document.querySelector('[data-sound="kick"]');
+        this.openhatSound = document.querySelector('[data-sound="openhat"]');
+        this.rideSound = document.querySelector('[data-sound="ride"]');
+        this.snareSound = document.querySelector('[data-sound="snare"]');
+        this.tinkSound = document.querySelector('[data-sound="tink"]');
+        this.tomSound = document.querySelector('[data-sound="tom"]');
     }
 
     onKeyDown(ev: KeyboardEvent): void {
@@ -42,13 +56,37 @@ export class App {
                 this.boomSound.currentTime = 0;
                 this.boomSound.play();
                 break;
-            case 's':
-                this.boomSound.currentTime = 0;
-                this.boomSound.play();
+            case 'w':
+                this.clapSound.currentTime = 0;
+                this.clapSound.play();
                 break;
-            case 'd':
+            case 'e':
                 this.hihatSound.currentTime = 0;
                 this.hihatSound.play();
+                break;
+            case 'a':
+                this.kickSound.currentTime = 0;
+                this.kickSound.play();
+                break;
+            case 's':
+                this.openhatSound.currentTime = 0;
+                this.openhatSound.play();
+                break;
+            case 'd':
+                this.rideSound.currentTime = 0;
+                this.rideSound.play();
+                break;
+            case 'z':
+                this.snareSound.currentTime = 0;
+                this.snareSound.play();
+                break;
+            case 'x':
+                this.tinkSound.currentTime = 0;
+                this.tinkSound.play();
+                break;
+            case 'c':
+                this.tomSound.currentTime = 0;
+                this.tomSound.play();
                 break;
         }
     }
