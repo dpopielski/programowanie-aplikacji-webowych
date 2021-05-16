2a6207cbd1bce15ca36d5451ceccad9cee10ad42
dpopielski <damianpopielski.dp@gmail.com>
2021-05-16 16:10:13 +0200

    init

---
+++ Projekt-lab4/src/app.ts
@@ -0,0 +1,10 @@
+export class App {
+    constructor() {
+        // const b = new Form()
+    }
+}
+export const AAA = 10;
+
+const hello = "yep, it's workin.jh adasd still sick";
+document.body.innerHTML = hello;
+const app = new App();
