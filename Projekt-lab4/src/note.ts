a153550d25df33fbbddd9f1345310cb29914d8bd
Damian Popielski <damianpopielski.dp@gmail.com>
2021-05-24 23:18:23 +0200

    new styles, noteItems

--- Projekt-lab4/src/note.ts
+++ Projekt-lab4/src/note.ts
@@ -1,25 +1,54 @@
 export class Note {
     noteTitle: string;
     noteText: string;
-    divEl: HTMLDivElement = document.createElement("div"); 
+    divEl: HTMLDivElement; 
     addBtn: HTMLButtonElement;
     notes = <HTMLDivElement>document.getElementById("notes");
 
     constructor() {
         this.addBtn = <HTMLButtonElement>document.getElementById("addBtn");
         this.addBtn.addEventListener("click", () => {
-            this.noteTitle = (<HTMLInputElement>document.getElementById("noteTitle")).value;
-            this.noteText = (<HTMLInputElement>document.getElementById("noteText")).value;
-            this.divEl.innerHTML = `
-            <h2>${this.noteTitle}</h2>
-            <p>${this.noteText}</p>
-            `;
+            this.noteTitle = (<HTMLInputElement>document.getElementById("noteTitle")).textContent;
+            this.noteText = (<HTMLInputElement>document.getElementById("noteText")).textContent;
+            this.createNote(this.noteTitle, this.noteText);
             this.notes.appendChild(this.divEl);
         });
+
+        
     }
 
-    createNote() {
-        
+    createNote(noteTitle: string, noteText: string) {
+        this.divEl = document.createElement("div");
+        this.divEl.className = "notes__item";
+        this.divEl.setAttribute('contenteditable', 'false');
+        this.divEl.setAttribute('aria-multiline', 'true');
+        this.divEl.setAttribute('role', 'textbox');
+        this.divEl.setAttribute('dir', 'ltr');
+        this.divEl.innerHTML = `
+            <h2>${this.noteTitle}</h2>
+            <span>${this.noteText}</span>
+        `;
+        this.divEl.style.border = "1px solid #fff";
+        this.divEl.style.minWidth = "50px";
+        this.divEl.style.display = "block";
+        this.divEl.style.padding = "10px";
+        this.divEl.style.letterSpacing = ".01428571em";
+        this.divEl.style.color = "#e8eaed";
+        this.divEl.style.outline = "none";
+        this.divEl.style.unicodeBidi = "isolate";
+        this.divEl.style.overflow = "visible";
+
+        // this.divEl.style.overflowWrap = "anywhere";
+        // this.divEl.style.overflow = "scroll";
+        // this.divEl.style.borderRadius = "10px";
+        // this.divEl.style.padding = "10px"
+        // this.divEl.style.minHeight = "80px"
+        // this.divEl.style.maxHeight = "200px"
+        // this.divEl.style.minWidth = "100px"
+        // this.divEl.style.maxWidth = "250px"
+    }
+    saveData(data: any) {
+        localStorage.setItem('divEl', JSON.stringify(data));
     }
 }
 
